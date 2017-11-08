70bc2e2367c2a329ec52b4a95ec381e14250a995
VanessaVanG <vkshiro@live.com>
2017-11-08 12:29:47 -0500

    Update twoples.py

--- twoples.py
+++ twoples.py
@@ -2,6 +2,21 @@
 Create a function named multiply that takes any number of arguments. Return the product (multiplied value) of all of the supplied arguments. The type of argument shouldn't matter.
 Slices might come in handy for this one.'''
 
+#Revised function
+def multiply(*nums):
+    total = 1
+    for num in nums:
+        total *= num
+    return total
+
+#If you use slices maybe? (the function is correct--but don't know if that's what he meant. https://teamtreehouse.com/community/slices-issue https://teamtreehouse.com/community/is-this-how-you-would-use-slices-in-the-twople-challenge
+def multiply(*nums):
+    total = nums[0]
+    for num in nums[1:]:
+        total *= num
+    return total
+
+#my original function
 def multiply(*nums):
     x = len(nums) - 1
     total = nums[x] 
@@ -9,4 +24,3 @@ def multiply(*nums):
         x -= 1
         total = total * nums[x]
     return total
-    
